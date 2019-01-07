90f3ef8e4a11059b792af5e7ae924a89d19e8d62
Víctor Román Archidona <vroman@users.noreply.github.com>
2019-01-07 19:52:21 +0100

    Every '#define RegX register' was commented. Remove them.

--- include/sys.h
+++ include/sys.h
@@ -66,18 +66,6 @@
 #undef IRC_UID
 #undef IRC_GID
 #endif
-/*
-#define Reg1 register
-#define Reg2 register
-#define Reg3 register
-#define Reg4 register
-#define Reg5 register
-#define Reg6 register
-#define Reg7 register
-#define Reg8 register
-#define Reg9 register
-#define Reg10 register
-i*/
 
 #define Reg1 
 #define Reg2
