80f8e7cfc3341fee3b89c6667f22e58ffa969410
Víctor Román Archidona <vroman@users.noreply.github.com>
2019-01-24 02:20:14 +0100

    Fix format type

--- src/ircd/s_ping.c
+++ src/ircd/s_ping.c
@@ -151,7 +151,7 @@ void send_ping(aClient *cptr)
   remote_addr.sin_family = AF_INET;
 
   gettimeofday(&tv, NULL);
-  sprintf((char *)cptr->confs, " %10u%c%6u", tv.tv_sec, '\0', tv.tv_usec);
+  sprintf((char *)cptr->confs, " %10lu%c%6u", tv.tv_sec, '\0', tv.tv_usec);
 
   Debug((DEBUG_SEND, "send_ping: sending [%s %s] to %s.%d on %d",
       (char *)cptr->confs, (char *)cptr->confs + 12,
